d6f83b299beddaf207316ac00eff2d32fec15a53
Darin Spivey <darin.spivey@logdna.com>
2021-02-11 16:37:16 -0500

    deps: eslint-config-logdna@4.0.2

--- test/bin.js
+++ test/bin.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const {test} = require('tap')
 const path = require('path')
 const {promisify} = require('util')
 const cp = require('child_process')
 const fs = require('fs')
+const {test} = require('tap')
 const bin = path.join(__dirname, '../bin/cmd.js')
 const exec = promisify(cp.exec)
 const readFile = promisify(fs.readFile)
@@ -15,8 +15,8 @@ test('bin', async (t) => {
       await exec(bin)
       t.fail('exec should have rejected')
     } catch (err) {
-      t.equal(err.code, 1, 'exited with code 1')
-      t.equal(err.stderr, 'Usage: config-doc <path to config>\n', 'stderr')
+      t.strictEqual(err.code, 1, 'exited with code 1')
+      t.strictEqual(err.stderr, 'Usage: config-doc <path to config>\n', 'stderr')
     }
   })
 
@@ -25,6 +25,6 @@ test('bin', async (t) => {
     const output_fp = path.join(__dirname, 'fixtures/output.md')
     const output = await readFile(output_fp, 'utf8')
     const {stdout} = await exec(`${bin} ${fixture}`)
-    t.equal(stdout, output, 'stdout is correct')
+    t.strictEqual(stdout, output, 'stdout is correct')
   })
 })
